2381c58ce59d93d5ac72b35d1725329b31344520
rzinkstok <rzinkstok@gmail.com>
2019-09-07 06:40:00 -0700

    First gates of A6 sheet 2

--- agc.srcs/sources_1/new/modules/a6_crosspoint_ii.v
+++ agc.srcs/sources_1/new/modules/a6_crosspoint_ii.v
@@ -2,9 +2,10 @@
 
 module a6_crosspoint_ii(
     // Inputs
-    T01, T01_, T02, T02_, T03, T04, T04_, T05, T06, T07, T08, T09, T10, T11, T12,
+    T01, T01_, T02, T02_, T03, T04, T04_, T05, T06, T06_, T07, T08, T09, T10, T11, T12,
     DV4_, DV376_, DV1376_, DIVSTG,
-    RBSQ, MP1_, MP3_, RADRG, T12USE_, RRPA, RB1F, BR1, BR1_, PHS4_, MONEX_, PTWOX, B15X,
+    RBSQ, MP1_, MP3_, RADRG, T12USE_, RRPA, RB1F, BR1, BR1_, BR2, BR12B_, PHS4_, MONEX_, PTWOX, B15X,
+    AUG0_, DIM0_, INCR0, DINC_, PINC,
     L01_, L02A_, L15A_,
     STBE, STBF,
     n1XP10, n2XP3, n2XP7, n3XP2, n3XP6, n3XP7, n4XP11, n5XP4, n5XP12, n5XP15, n5XP21, n5XP28, n6XP5, n6XP8, n7XP19, n9XP5,
@@ -25,15 +26,17 @@ module a6_crosspoint_ii(
     A06_1_WG_,
     
     DVXP1, L2GD_, ZIP, ZIPCI, ZAP, ZAP_, MCRO_, RCH_, TSGU_, CLXC, WQ_, MONEX, TWOX, BXVX, PIFL_, CGMC,
+    n6XP10,
     
     // Input
     SIM_CLK
     );
     
     input wire SIM_CLK;
-    input wire T01, T01_, T02, T02_, T03, T04, T04_, T05, T06, T07, T08, T09, T10, T11, T12;
+    input wire T01, T01_, T02, T02_, T03, T04, T04_, T05, T06, T06_, T07, T08, T09, T10, T11, T12;
     input wire DV4_, DV376_, DV1376_, DIVSTG;
-    input wire RBSQ, MP1_, MP3_, RADRG, T12USE_, RRPA, RB1F, BR1, BR1_, PHS4_, MONEX_, PTWOX, B15X;
+    input wire RBSQ, MP1_, MP3_, RADRG, T12USE_, RRPA, RB1F, BR1, BR1_, BR2, BR12B_, PHS4_, MONEX_, PTWOX, B15X;
+    input wire AUG0_, DIM0_, INCR0, DINC_, PINC;
     input wire L01_, L02A_, L15A_;
     input wire STBE, STBF;
     input wire n1XP10, n2XP3, n2XP7, n3XP2, n3XP6, n3XP7, n4XP11, n5XP4, n5XP12, n5XP15, n5XP21, n5XP28, n6XP5, n6XP8, n7XP19, n9XP5;
@@ -53,6 +56,7 @@ module a6_crosspoint_ii(
     output wire A06_1_WG_;
     
     output wire DVXP1, L2GD_, ZIP, ZIPCI, ZAP, ZAP_, MCRO_, RCH_, TSGU_, CLXC, WQ_, MONEX, TWOX, BXVX, PIFL_, CGMC;
+    output wire n6XP10;
     
     
     /**************************
@@ -298,6 +302,34 @@ module a6_crosspoint_ii(
     nor_1 #(1'b0) NOR40256(NOR40256_out,    NOR40255_out,                                               SIM_CLK);
     nor_1 #(1'b0) NOR40257(NOR40257_out,    NOR40256_out,                                               SIM_CLK);
     nor_1 #(1'b0) NOR40258(NOR40258_out,    NOR40257_out,                                               SIM_CLK);
+    
+    /**************************
+    *
+    *  Module A6 sheet 2
+    *  Sheet number 2005263/2
+    *
+    **************************/
+    
+    wire NOR40302_out;
+    wire NOR40303_out;
+    wire NOR40304_out;
+    wire NOR40305_in;
+    wire NOR40305_out;
+    wire NOR40306_out;
+    wire NOR40307_out;
+    
+    
+    
+    // NOR40301 not used
+    
+    // 6XP10
+    nor_2 #(1'b0) NOR40302(NOR40302_out,    BR1,            AUG0_,                                      SIM_CLK);
+    nor_2 #(1'b0) NOR40303(NOR40303_out,    DIM0_,          BR12B_,                                     SIM_CLK);
+    nor_3 #(1'b0) NOR40304(NOR40304_out,    NOR40302_out,   NOR40303_out,   INCR0,                      SIM_CLK);
+    nor_2 #(1'b0) NOR40306(NOR40306_out,    PINC,           NOR40307_out,                               SIM_CLK);
+    assign NOR40305_in = NOR40304_out & NOR40305_out;
+    nor_2 #(1'b0) NOR40307(NOR40307_out,    BR12B_,         DINC_,                                      SIM_CLK);
+    nor_2 #(1'b0) NOR40305(n6XP10,          T06_,           NOR40305_in,                                SIM_CLK);
     
     
 endmodule
